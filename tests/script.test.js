e430fc6f016585de34e072eec3817cbd96201b30
robby Siddiqui <robbysiddiqui@gmail.com>
2025-11-11 16:17:37 -0500

    changed project

--- tests/script.test.js
+++
@@ -1,7 +0,0 @@
-const { formatPhoneNumber } = require('../script.js');
-
-test('formatPhoneNumber formats a numeric string correctly', () => {
-  const input = { value: '1234567890' };
-  formatPhoneNumber(input);
-  expect(input.value).toBe('(123) 456-7890');
-});
